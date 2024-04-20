2164be540fd431d297503823c74af33f216ce966
Kai Aragaki <adamaragaki@gmail.com>
2024-04-20 13:29:49 -0400

    add get_associated_bamfiles and doc

---
+++ R/get_associated_bamfiles.R
@@ -0,0 +1,22 @@
+#' Get BAM files associated with samples or analyses
+#'
+#' @param name character vector of either sample or analysis names
+#' @param type character, either "sample" or "analysis"
+#'
+#' @details You can get a list of analysis names using the `list_analyses`
+#'   function. It's unclear if there's a way to get a list of sample names from
+#'   the API.
+#'
+#' @export
+get_associated_bamfiles <- function(name, type = c("analysis", "sample")) {
+  type <- rlang::arg_match(type)
+  end <- "https://ionreporter.thermofisher.com/api/v1/getAssociatedBamfiles"
+  httr2::request(end) |>
+    httr2::req_headers(
+      `Content-Type` = "application/x-www-form-urlencoded",
+      Authorization = get_key()
+    ) |>
+    httr2::req_url_query(name = name, type = type, .multi = "comma") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
