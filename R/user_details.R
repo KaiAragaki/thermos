e3e72e88b494557118147426cab24067d5573f21
Kai Aragaki <adamaragaki@gmail.com>
2024-04-22 11:06:53 -0400

    make auth a parameter
    Useful if you want to check on a user that isn't you

--- R/user_details.R
+++ R/user_details.R
@@ -1,10 +1,13 @@
-get_user_details_with_auth <- function() {
+get_user_details_with_auth <- function(auth = NULL) {
+  if (is.null(auth)) {
+    auth <- get_key()
+  }
   default_req("getUserDetailsWithAuth") |>
     httr2::req_method("POST") |>
     httr2::req_headers(
       `Content-Type` = "application/x-www-form-urlencoded"
     ) |>
-    httr2::req_url_query(auth = get_key()) |>
+    httr2::req_url_query(auth) |>
     httr2::req_perform() |>
     httr2::resp_body_json()
 }
