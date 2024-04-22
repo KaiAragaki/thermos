62eaaf12c14e1fcf90d3e88d3bbe3a8bb6c161b4
Kai Aragaki <adamaragaki@gmail.com>
2024-04-22 15:11:33 -0400

    finish final_report

--- R/final_report.R
+++ R/final_report.R
@@ -1,5 +1,20 @@
-finalreport <- function() {
-  default_req("qcreport") |>
-    httr2::req_headers(Authorization = get_key()) |>
-    httr2::req_url_query(format = "json")
+finalreport <- function(type = c("sample", "analysis"),
+                        name,
+                        id = NULL,
+                        start_date = NULL,
+                        end_date = NULL,
+                        view = NULL) {
+  type <- rlang::arg_match(type)
+  default_req("finalreport") |>
+    default_headers() |>
+    httr2::req_url_query(
+      format = "json",
+      type = type,
+      name = name,
+      id = id,
+      start_date = start_date,
+      end_date = end_date,
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
 }
