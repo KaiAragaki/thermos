0cfd982de9bfcd0ba91b2c190ef90f18f8d073d9
Kai Aragaki <adamaragaki@gmail.com>
2024-04-22 11:56:16 -0400

    add user_details, even though it's nonfunctional

--- R/user_details.R
+++ R/user_details.R
@@ -11,3 +11,14 @@ get_user_details_with_auth <- function(auth = NULL) {
     httr2::req_perform() |>
     httr2::resp_body_json()
 }
+
+get_user_details <- function(user_name, password) {
+  default_req("getUserDetails") |>
+    httr2::req_method("POST") |>
+    httr2::req_headers(
+      `Content-Type` = "application/x-www-form-urlencoded"
+    ) |>
+    httr2::req_url_query(userName = user_name, password = password) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
